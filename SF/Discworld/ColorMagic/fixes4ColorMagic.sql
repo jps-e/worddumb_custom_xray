505f8b2319402610c83307154585f99551baf9ab
jps-e <jps-e@users.noreply.github.com>
2026-01-11 14:42:00 -0800

    "Add Color of Magic" specific files.
    worddumb-custom-x-ray.cull contains worddumb-custom-x-ray.json
    format data to omit entities with only 1 or 2 instances in the
    text of the book. These are overwhemingly bogus detected entities
    and anyway if there is one instance context from elsewhere in the
    book doesn't exist. If there are two, the other instance is usually
    easily found with a search. Entities listed in the file "uncull"
    have been removed from the generate worddumb-custom-x-ray.cull
    file. If it has been regenerated then uncull should be used to
    fix worddumb-custom-x-ray.cull, which is supposed to be inserted
    into a copy of the series wide worddumb-custom-x-ray.json
    
    uncull contains a list of entities in worddumb-custom-x-ray.cull
    that should be removed.
    
    fixes4ColorMagic.sql is a set of SQL statements to fix an
    entity_description that can't be done by worddumb-custom-x-ray.json
    usually because there is a punctuation character in the entity
    name.

---
+++ SF/Discworld/ColorMagic/fixes4ColorMagic.sql
@@ -0,0 +1,20 @@
+UPDATE "entity_description"
+ SET text=
+ "Great A'Tuin is the Giant Star Turtle (of the fictional species Chelys galactica) who travels through the Discworld universe's space, carrying four giant elephants (named Berilia, Tubul, Great T'Phon, and Jerakeen) who in turn carry the Discworld."
+ where source_wildcard like "%Tuin";
+UPDATE "entity_description"
+ SET text=
+ "Lio!rt Dragonlord is the stranger and more rash of the two sons of the late Greicha, ruler of Wyrmberg."
+ where source_wildcard like "%Dragonlord";
+UPDATE "entity_description"
+ SET text=
+ "Great T'Phon is one of the four mighty World Elephants that stand upon the carapace of Great A'Tuin the World Turtle, and upon whose massive shoulders revolves the Discworld itself. The others are Jerakeen, Tubul, and ."
+ where source_wildcard like "%Phon";
+UPDATE "entity_description"
+ SET text=
+ "Time is the most secretive among the more important anthropomorphic personifications of the Discworld."
+ where source_wildcard = "Time";
+UPDATE "entity_description"
+ SET text=
+ "K!sdra is a dragon rider of the Wyrmberg. Bronze Psepha is among the dragons that he rides."
+ where source_wildcard like "%sdra";
