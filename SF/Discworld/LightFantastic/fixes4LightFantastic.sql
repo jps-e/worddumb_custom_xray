42fc453a32dc080e541edfb05bc93b29382f39d6
jps-e <jps-e@users.noreply.github.com>
2026-01-16 16:15:02 -0800

    Add "Light Fantastic" files.

---
+++ SF/Discworld/LightFantastic/fixes4LightFantastic.sql
@@ -0,0 +1,4 @@
+UPDATE "entity_description"
+ SET text=
+ "Great A'Tuin is the Giant Star Turtle (of the fictional species Chelys galactica) who travels through the Discworld universe's space, carrying four giant elephants (named Berilia, Tubul, Great T'Phon, and Jerakeen) who in turn carry the Discworld."
+ where source_wildcard like "%Tuin";
